e733c8db43c2d135a3d8acca12b7ff686d53fbaf
Caroline Pantofaru <pantofaru@willowgarage.com>
2012-04-24 00:12:22 +0000

    included ros/console.h for fuerte
    git-svn-id: https://code.ros.org/svn/wg-ros-pkg/stacks/people/trunk@54547 7275ad9f-c29b-430a-bdc5-66f4b3af1622

--- face_detector/include/face_detector/faces.h
+++ face_detector/include/face_detector/faces.h
@@ -53,6 +53,7 @@
 
 #include "image_geometry/stereo_camera_model.h"
 #include "ros/time.h"
+#include <ros/console.h>
 #include <boost/thread/mutex.hpp>
 #include <boost/bind.hpp>
 #include <boost/thread/thread.hpp>
