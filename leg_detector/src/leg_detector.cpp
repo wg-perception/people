20257a42fc30b5ce4b01626f42c0289b71d0125a
Karl Damkjær Hansen <kdh@es.aau.dk>
2018-04-30 18:22:05 +0200

    Update leg_detector to OpenCV3 (#53)
    * [kinetic] Update Travis config to run on Ubuntu 16.04 via Docker.
    
    * [test] Missing dependency.
    
    * [test] Cleaner namespace assignment.
    [face_detector][test common] Fix warning in .launch format.
    Fix namespace in testcases.
    Increase retry. .bag-based perceptional tests are hard to deterministic, so it makes sense to do this.
    Set param use_sim_time.
    [face_detector][test] hztest can be used for measureing no publishment. hzerror is not needed in that case.
    [face_detector][test] Specify test name. Pass all args.
    [face_detector][test] pass_all_args might require all args kept out of include tag.
    [face_detector][test] Specify test-name.
    [face_detector][test] Disable all tests other than rgbd. I couldn't figure out how to use launches other than face_detector.rgbdlaunch, after hours of trial on local machine. I leave that up to someone else.
    [face_detector][test] Detection using rgbd works with this config.
    [face_detector][test] Shorter test duration (bag files are less than 7 seconds).
    [face_detector][test] Shorter test_duration. We only need a second or so as long as we can confirm the designated topic is being published.
    [face_detector][test] rosbag play with clock and loop option.
    
    * [face_detector] an obtuse fix to hardcoded path for Xenial with OpenCV 3.1.0.
    
    * [face_detector][test common] Use newly introduced publishtest.
    Since the publish rate of the detected face varies every second because one of the persons in the bag file is moving, [publishtest](http://wiki.ros.org/rostest/Nodes#publishtest) should suit the best in this case.
    
    [face_detector][test common] Enable substitution in rosparam tag.
    
    * Fix CvMat change in OpenCV3
    
    The cv::ml package has been rewritten. I just changed the code to
    accomodate the new api, maybe this does not conform to the way the new
    cv::ml should be used...
    
    * Fix missing fuzzy prediction from opencv2
    
    In opencv3 the predict_prob method was removed from cv::ml::RTrees.
    
    * Remove dependency on people_msgs generation
    
    The dependency on the people_msgs package should be enough. This removes
    warnings for the leg_detector and people_tracking_filter when building
    with catkin_make_isolated and catkin build.
    
    * Revert "Remove no build packages from metapackage."
    
    This reverts commit 0bda254faade07474ee11dc952b0664ea4d1260e.
    
    * Save Isaac from getting emails about failed builds.
    
    Isaac Saito has been working hard to make the Travis CI work with
    Kinetic. Removing his email from ravis.yml, to save his inbox from
    errors due to other issues.
    
    * Update hardcoded location of opencv
    
    Version changed to 3.2.0, unfortunately we need to hardcode the
    location for now.

--- leg_detector/src/leg_detector.cpp
+++ leg_detector/src/leg_detector.cpp
@@ -244,7 +244,8 @@ public:
 
   int mask_count_;
 
-  cv::ml::RTrees forest;
+  // cv::ml::RTrees forest;
+  cv::Ptr<cv::ml::RTrees> forest;
 
   float connected_thresh_;
 
@@ -286,8 +287,10 @@ public:
   {
     if (g_argc > 1)
     {
-      forest.load(g_argv[1]);
-      feat_count_ = forest.get_active_var_mask()->cols;
+      forest = cv::ml::RTrees::create();
+      cv::String feature_file = cv::String(g_argv[1]);
+      forest = cv::ml::StatModel::load<cv::ml::RTrees>(feature_file);
+      feat_count_ = forest->getVarCount();
       printf("Loaded forest with %d features: %s\n", feat_count_, g_argv[1]);
     }
     else
@@ -684,7 +687,7 @@ public:
     processor.splitConnected(connected_thresh_);
     processor.removeLessThan(5);
 
-    CvMat* tmp_mat = cvCreateMat(1, feat_count_, CV_32FC1);
+    cv::Mat tmp_mat = cv::Mat(1, feat_count_, CV_32FC1);
 
     // if no measurement matches to a tracker in the last <no_observation_timeout>  seconds: erase tracker
     ros::Time purge = scan->header.stamp + ros::Duration().fromSec(-no_observation_timeout_s);
@@ -725,9 +728,14 @@ public:
       vector<float> f = calcLegFeatures(*i, *scan);
 
       for (int k = 0; k < feat_count_; k++)
-        tmp_mat->data.fl[k] = (float)(f[k]);
+        tmp_mat.data[k] = (float)(f[k]);
 
-      float probability = forest.predict_prob(tmp_mat);
+      // Probability is the fuzzy measure of the probability that the second element should be chosen,
+      // in opencv2 RTrees had a method predict_prob, but that disapeared in opencv3, this is the
+      // substitute.
+      float probability = 0.5 -
+                          forest->predict(tmp_mat, cv::noArray(), cv::ml::RTrees::PREDICT_SUM) /
+                          forest->getRoots().size();
       Stamped<Point> loc((*i)->center(), scan->header.stamp, scan->header.frame_id);
       try
       {
@@ -841,8 +849,6 @@ public:
       }
     }
 
-    cvReleaseMat(&tmp_mat);
-    tmp_mat = 0;
     if (!use_seeds_)
       pairLegs();
 
